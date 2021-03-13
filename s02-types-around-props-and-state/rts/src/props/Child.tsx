58a9e842f94b47877e115027e3baeb852faa090b
rene <renegmed@yahoo.com>
2021-03-13 05:20:16 -0500

    s02-types and props and state: explicit component type annotations

--- s02-types-around-props-and-state/rts/src/props/Child.tsx
+++ s02-types-around-props-and-state/rts/src/props/Child.tsx
@@ -6,3 +6,8 @@ export const Child = ({ color }: ChildProps) => {
   return <div>{color}</div>;
 };
 
+export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
+  return <div>{color}</div>;
+};
+
+
